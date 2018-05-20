77df5cdd6736dcb7fdc15bbf983e4f2dc27d5db5
Christoffer Corfield Aakre <Corfieldaakre@outlook.com>
2018-05-20 22:39:00 +0200

    exported styles form app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+export const styles = {
+  container: {},
+};
+
 export const App = () => (
-  <div>This is the root (App.js) component. Use it to render other components/routes</div>
+  <div style={styles.container}>
+    This is the root (App.js) component. Use it to render other components/routes
+  </div>
 );
 
 export default App;
